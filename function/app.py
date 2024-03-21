e62e28f41759e983cc2edc3243c57908c25afdb7
danielwilson1227 <65043123+danielwilson1227@users.noreply.github.com>
2024-03-21 16:16:38 -0400

    updates

--- function/app.py
+++ function/app.py
@@ -1,14 +1,14 @@
 # import pandas as pd
 # from validation import account_validation as validationLayer
 # from testing import account_testing as testingLayer
-import requests
+import pandas as pd
 
 def lambda_handler(event, context):
     print("test")
-    # d = {'col1': [1, 2], 'col2': [3, 4]}
-    # df = pd.DataFrame(data=d)
-    # print(df)
-    # print('Done x1.2')
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    print(df)
+    print('Done x1.2')
 
     # if validationLayer.validateAcct(event['AcctNo']) == "Pass":
     #     print("Passed")
