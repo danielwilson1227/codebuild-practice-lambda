4007aba6465c04553290c3c18aad2ab5e8133198
danielwilson1227 <65043123+danielwilson1227@users.noreply.github.com>
2024-03-21 15:33:20 -0400

    fix

--- function/app.py
+++ function/app.py
@@ -1,18 +1,20 @@
-import pandas as pd
-from validation import account_validation as validationLayer
-from testing import account_testing as testingLayer
+# import pandas as pd
+# from validation import account_validation as validationLayer
+# from testing import account_testing as testingLayer
+import requests
 
 def lambda_handler(event, context):
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.2')
+    # d = {'col1': [1, 2], 'col2': [3, 4]}
+    # df = pd.DataFrame(data=d)
+    # print(df)
+    # print('Done x1.2')
 
-    if validationLayer.validateAcct(event['AcctNo']) == "Pass":
-        print("Passed")
+    # if validationLayer.validateAcct(event['AcctNo']) == "Pass":
+    #     print("Passed")
 
-    if testingLayer.testAcct(event['AcctNo']) == "Acct no is 10 characters":
-        print("Passed")
+    # if testingLayer.testAcct(event['AcctNo']) == "Acct no is 10 characters":
+    #     print("Passed")
+  
 
 
 
