0248fad004379ab0b996e87f25bb41deac888035
danielwilson1227 <65043123+danielwilson1227@users.noreply.github.com>
2024-03-21 12:20:44 -0400

    added layer

--- function/app.py
+++ function/app.py
@@ -1,11 +1,15 @@
 import pandas as pd
-
+import account_validation as validationLayer
 
 def lambda_handler(event, context):
     d = {'col1': [1, 2], 'col2': [3, 4]}
     df = pd.DataFrame(data=d)
     print(df)
     print('Done x1.2')
+
+    if validationLayer.validateAcct(event['AcctNo']) == "Pass":
+        print("Passed")
+
 
 
 
